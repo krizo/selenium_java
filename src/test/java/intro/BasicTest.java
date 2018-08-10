9c87a82da090ff47d4bcc45445f0e2e497eb9197
Krzysztof Bober <krzysztof.bober@getbase.com>
2018-08-10 09:50:07 +0200

    setting up driver and cleaning it after test

--- src/test/java/intro/BasicTest.java
+++ src/test/java/intro/BasicTest.java
@@ -6,15 +6,22 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class BasicTest {
     private ExpectedCondition<Boolean> pageTitleStartsWith(final String searchString) {
         return driver -> driver.getTitle().toLowerCase().startsWith(searchString.toLowerCase());
     }
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setupTest() {
+        driver = new ChromeDriver();
+    }
 
     private void googleExampleThatSearchesFor(final String searchString) {
-        WebDriver driver = new ChromeDriver();
         driver.get("http://google.com");
         WebElement searchField = driver.findElement(By.name("q"));
         searchField.clear();
@@ -33,5 +40,10 @@ public class BasicTest {
     @Test
     public void googleMilkExample() {
         googleExampleThatSearchesFor("Milk!");
+    }
+
+    @AfterTest
+    public void cleanUp() {
+        driver.quit();
     }
 }
